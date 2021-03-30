8b176e0b3024479442331473e0439b07c3755965
woojin624 <54699702+woojin624@users.noreply.github.com>
2021-03-30 14:34:55 +0900

    http test for aws

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the homepage');
+  res.sendFile(__dirname + '/index.html');
 });
 
-const PORT = process.env.PORT || 3000;
+app.post('/add', function (req, res) {
+  console.log(req.body);
+  res.send('전송완료');
+});
+
+//
+//
+//
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log('Wazzappppp');
 });
