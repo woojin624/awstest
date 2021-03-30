1cde0690bf1deb4f1f481841852281b0f218f0bf
woojin624 <54699702+woojin624@users.noreply.github.com>
2021-03-30 12:54:41 +0900

    app.js -> server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the homepage');
 });
 
-const port = process.env.port || 3000;
-app.listen(port, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log('Wazzappppp');
 });
